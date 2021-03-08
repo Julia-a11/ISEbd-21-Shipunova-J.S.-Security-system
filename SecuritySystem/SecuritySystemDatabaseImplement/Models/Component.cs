f58d89169aa1fe6afb51c4d2d2267972dc07c9c3
Julia-a11 <68960968+Julia-a11@users.noreply.github.com>
2021-03-08 17:47:43 +0400

    ready

---
+++ SecuritySystem/SecuritySystemDatabaseImplement/Models/Component.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SecuritySystemDatabaseImplement.Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ComponentName { get; set; }
+
+        [ForeignKey("ComponentId")]
+        public virtual List<SecureComponent> SecureComponents { get; set; }
+    }
+}
---
+++ SecuritySystem/SecuritySystemDatabaseImplement/Models/Secure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SecuritySystemDatabaseImplement.Models
+{
+    public class Secure
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SecureName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey("SecureId")]
+        public virtual List<SecureComponent> SecureComponents { get; set; }
+
+        [ForeignKey("SecureId")]
+        public virtual List<Order> Orders { get; set; }
+    }
+}
