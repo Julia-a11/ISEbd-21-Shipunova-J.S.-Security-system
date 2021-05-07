f7579bedc5dd40cfc782627919079b2ad73328e6
Julia-a11 <68960968+Julia-a11@users.noreply.github.com>
2021-05-07 12:34:29 +0400

    fix

--- SecuritySystem/SecuritySystemBusinessLogic/BusinessLogics/OrderLogic.cs
+++ SecuritySystem/SecuritySystemBusinessLogic/BusinessLogics/OrderLogic.cs
@@ -103,7 +103,7 @@ namespace SecuritySystemBusinessLogic.BusinessLogics
                 Count = order.Count,
                 Sum = order.Sum,
                 DateCreate = order.DateCreate,
-                DateImplement = DateTime.Now,
+                DateImplement = order.DateImplement,
                 Status = OrderStatus.Готов
             });
         }
